73bf76e98c3de7795a1aaf2d21a9a8794cbd4532
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2024-01-04 16:54:04 +0900

    2023.01.04 6th

--- 01.basic/script/8_JS.js
+++ 01.basic/script/8_JS.js
@@ -1,8 +1,8 @@
 function variableConsole1(){
     var x = 10;
     var y = "10";
-    console.log( x + y );
-}
+    console.log( x + y );}
+
 
 
 function variableConsole2(){
@@ -13,18 +13,57 @@ function variableConsole2(){
         total += arr2[i];
     console.log(arr);
 
+
+    arr2.push(50);
+    arr.forEach((a) => console.log(a))
+
     // 콘솔로 출력
     console.log("total : " + total);
 
     // pop up으로 출력: 확인 누르기전 까지 이후 코드 실행x
     // window.alert("total : " + total); ( 윈도우 생략가능)
     alert("total : " + total);
-    document.write("total :  " + total); // 쓸일이 없다. 
-
+    // document.write("total :  " + total); // 쓸일이 없다. 
 }
 
 function variableConsole3(){
+    var arr = [10, 20, () => {console.log("hello world");}];
+    arr[2]();}
 
-    var arr = [10, 20, 30, function(){console.log("hello world")} ]
+
+function varExample(){
+    // var는 같은 변수명에 재 선언과 재할당 가능.
+    var x = 10;
+    var x = 20;
+    console.log("var: " + x)}
+
+
+function letExample(){
+    // let는 같은 변수명에 재할당만 가능
+    let x = 10;
+    x = 20;
+    console.log("let: " + x)}
+
+    
+function constExample(){
+    // const는 변수명에 재선언과 재할당 불가능 
+    const x = 10;
+    x = 20;
+    console.log("const: " + x)}
+
+
+function changeTextById(){
+    document.getElementById("change1").innerHTML = "변경완료";
+    document.getElementById("change1").style.fontSize = "30px";}
+
+
+function chageTextByClass(){
+    var arr = document.getElementsByClassName("class1");
+    console.log(arr);
+    for(i = 0; i < arr.length; i++){
+        arr[i].innerHTML = "변경완료";
+        arr[i].style.fontSize = "20px";
+        arr[i].style.color = "red";
+    }
 
 }
---
+++ 01.basic/script/9_JS.js
@@ -0,0 +1,60 @@
+function showObject(){
+    // 리터럴 방식
+    var person = {
+        name: "hong", 
+        age: 20, 
+        email: "se@jong.com",
+    };
+    console.log(person);
+
+    person.name = "kim";
+    person.password = 4567;
+    console.log(person);
+
+    var str = 
+        "이름:" + person.name + "<br>" +
+        " 나이:" + person.age + "<br>" +
+        " email:" + person.email;
+
+    document.getElementById("result1").innerHTML = str;
+
+}
+
+
+
+function convertType(){
+    //  숫자 클래스 :number, 문자클래스 :String, 참 거짓 : 
+
+    var numStr = "10";
+    var str = "문자열";
+    var output = "Number 변환 : " + Number(numStr) + "<br>"
+            + "String 변환 : " + String(10) + "<br>"
+            + "Boolean 변환 : " + Boolean(0) + "<br>";
+            
+    document.getElementById("result2").innerHTML = output;
+    
+}
+
+function showNaN(){
+    var output = "10" - "문자열";
+    console.log(output);
+    console.log(typeof output);
+    document.getElementById("result3").innerHTML = output;
+
+}
+
+
+function showNull(){
+    var output1;
+    var output2 = null;
+    var output =    
+                "output1의 타입은 " + (typeof output1) + "<br>" +
+                "output2의 타입은 " + (typeof output2) + "<br>" + 
+                "output1 == output2: " + (output1 == output2) + "<br>" + 
+                "output2 === output2:  " + (output2 === output2) + "<br>"  ;
+
+
+    console.log(output);
+    document.getElementById("result4").innerHTML = output;
+
+}
