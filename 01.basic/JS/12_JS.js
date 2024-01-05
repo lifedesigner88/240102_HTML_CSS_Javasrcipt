a487229353a7a03af1c66ee332ef38cd7c2b537b
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2024-01-05 14:53:56 +0900

    2024.01.05 Refactor callback functions and update HTML buttons
    This commit considerably expands the callback examples in '12_JS.js' and properly refactors the 'processUserInput' function for easier readability and improved functionality. Also, '12_JS_callback.html' has been modified to include buttons for the new callback examples, improving the user interface and overall interaction with the page.

--- 01.basic/JS/12_JS.js
+++ 01.basic/JS/12_JS.js
@@ -4,12 +4,40 @@ function callBackExample1(){
 
 
 function callBackExample2(){
-    processUserInput(function(){
-
-    })
+    processUserInput((name) => console.log(`콜백1 console 안녕하세요. ${name} 님`));
 }
 
-function processUserInput(callback){
-    let name = promt("이름을 입력해주세요");
 
+function callBackExample3(){
+    processUserInput((name) => alert(`콜백 2 alert 안녕하세요. ${name} 님`));
+}
+
+
+function processUserInput(callback){
+    let name = prompt("이름을 입력해주세요");
+    callback(name)
+    
+}
+
+function callBackExample4(){
+    const numbers = [1, 2, 3, 4, 5];
+    numbers.forEach(a => console.log(a));
+    console.log("hello world")
+}
+
+
+function callBackExample5(){
+    // setTimeOut 함수는 대표적인 JS의 비동기 함수(콜백 함수 처리)
+    // 그외 네트워크 작업을 수행하는 많은 함수가 비동기 함수로 설계
+    setTimeout(()=>
+        console.log("wait 2000 1"),2000);
+    
+    console.log("hello world2");
+    
+    setTimeout(()=>
+        console.log("wait 1000 3"),1000);
+    
+    console.log("hello world4");
+
+    
 }
