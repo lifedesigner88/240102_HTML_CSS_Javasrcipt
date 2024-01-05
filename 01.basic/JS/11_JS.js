8630f498f5bf7c6e88d5c7e700b94f08cf675b89
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2024-01-05 12:48:14 +0900

    Update CSS styling, create new JS features and update HTML structure
    The commit includes adding new CSS styles for elements like input, buttons, and headers. The HTML structure in `11_JS_function.html` was updated with new sections and responsibilities for the buttons, while new JavaScript callback functions were added and featured in a new HTML file. Two new JavaScript files were created: `12_JS.js` and `12_JS_callback.html`. This commit helps to enhance user interactivity.

--- 01.basic/JS/11_JS.js
+++ 01.basic/JS/11_JS.js
@@ -1,3 +1,88 @@
-function basicFunction(){
+// 03. 항목 추가 제거 시스템
+
+let itemCount = 2;
+function createItem() {
+    const htmlContent = `
+        <div id="content-div-${itemCount}">
+            <label for="content-${itemCount}"> 항목 ${itemCount}  
+                <input type="text" id="content-${itemCount}"> 
+                <button onclick="deleteContent(${itemCount})">Delete</button>
+            </label><br>
+        </div>
+    `;
+    itemCount++;
+    return htmlContent;
+}
+
+function addContent() {
+    document.getElementById("addContent").innerHTML += createItem();
+}
+
+function deleteContent(id) {
+    const contentDiv = document.getElementById(`content-div-${id}`);
+    contentDiv.remove();
+    itemCount--;
+}
+
+
+
+// 02. 계산기 관련내용
+function calculate() {
+    const inputs = document.getElementsByClassName("inputs");
+    const a = Number(inputs[0].value);
+    const b = Number(inputs[1].value);
+    const oper = document.getElementById("oper").value;
+    let result;
+
+    if (oper === '+') result = a + b;
+    else if (oper === '-') result = a - b;
+    else if (oper === '*') result = a * b;
+    else if (oper === '/') result = a / b;
+
+    document.getElementById("result6").innerHTML = result;
+}
+
+
+
+
+
+// 01. 함수 관련 내용
+function basicFunction1(){
     alert("함수호출완료");
 }
+
+function basicFunction2(){
+    alert(makeString3());
+}
+
+
+function makeString1() {
+    return "hello world1"
+}
+
+const makeString2 = function () {return "hello world2"};
+
+const makeString3 = () => "hello world3";
+
+function basicFunction3(name='Guest'){
+    document.getElementById("result3").innerHTML = name;
+}
+
+function basicFunction4(a,b){
+    alert(a+b);
+}
+
+
+function basicFunction5(...nums){
+    const total = nums.reduce((a, b) => a + b, 100);
+    alert("합계 : " + total);
+}
+
+
+
+
+
+
+
+
+
---
+++ 01.basic/JS/12_JS.js
@@ -0,0 +1,15 @@
+function callBackExample1(){
+    alert("hello");
+}
+
+
+function callBackExample2(){
+    processUserInput(function(){
+
+    })
+}
+
+function processUserInput(callback){
+    let name = promt("이름을 입력해주세요");
+
+}
